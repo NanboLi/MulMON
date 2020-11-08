ee700c3bef6d9c4af6f8569b294a91024499b5e4
NanboLi <linanbo2008@gmail.com>
2020-11-08 13:31:49 +0000

    scripts added

---
+++ scripts/train_clevr_parallel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# makde sure the below path are correctly configured before run the project
+data_path=../data/MultiObj/clevr
+repo_path=.
+log_path=${repo_path}/logs
+
+python train_parallel.py --arch aug_MulMON --datatype clevr_aug --work_mode training \
+--input_dir ${data_path} --output_dir ${log_path} --resume_epoch 4000 \
+--batch_size 4 --epochs 5000 --step_per_epoch 300 --optimiser Adam --lr_rate 0.0002 --seed 0 \
+--num_slots 7 --pixel_sigma 0.1 --temperature 0.0 --latent_dim 16 --view_dim 5 --min_sample_views 1 --max_sample_views 6 --num_vq_show 5 \
+--query_nll 1.0 --exp_nll 1.0 --exp_attention 1.0 --kl_latent 1.0 --kl_spatial 1.0 \
+--nodes 1 --gpus 2 --gpu_start 0 --master_port '29500' --use_bg
---
+++ scripts/train_jaco.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# makde sure the below path are correctly configured before run the project
+data_path=../data/MultiObj/GQN
+repo_path=.
+log_path=${repo_path}/logs
+
+python train.py --arch jaco_MulMON --datatype gqn_jaco --work_mode training \
+--input_dir ${data_path} --output_dir ${log_path} \
+--batch_size 8 --epochs 2000 --step_per_epoch 300 --optimiser Adam --lr_rate 0.0002 --seed 0 \
+--num_slots 7 --pixel_sigma 0.1 --temperature 0.0 --latent_dim 16 --view_dim 7 --min_sample_views 1 --max_sample_views 6 --num_vq_show 5 \
+--query_nll 1.0 --exp_nll 1.0 --exp_attention 1.0 --kl_latent 1.0 --kl_spatial 1.0 \
+--nodes 1 --gpus 1 --gpu_start 1 --master_port '29500' --use_bg
